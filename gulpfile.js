06b43cb07a4d6d275f20b56502b1aeede25571d2
miway <miway@cntmac281.local>
2017-01-13 19:24:02 +0900

    一旦完成(csvTojson => tmp展開)

--- gulpfile.js
+++ gulpfile.js
@@ -5,10 +5,21 @@ var gulp = require('gulp'),
     rename = require('gulp-rename'),
     ejs = require('gulp-ejs'),
     gutil = require('gulp-util'),
-    jsonD = './dev/csv/test.json',
+    convert = require('gulp-convert'),
+    jsonD = './dev/json/test.json',
     tmpD = './dev/tmp/index.ejs',
     json = JSON.parse(fs.readFileSync(jsonD)),
     pages = json.pages;
+
+// csv => json
+gulp.task('csv2json', function() {
+  gulp.src(['./dev/csv/*.csv'])
+    .pipe(convert({
+        from: 'csv',
+        to: 'json'
+    }))
+    .pipe(gulp.dest('./dev/json'))
+});
 
 // ejs
 gulp.task('ejs', function() {
